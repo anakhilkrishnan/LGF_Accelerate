fa3e1d308b85fe09486e59c70f9d872f9289df2b
dragvangg <kakhil@iisc.ac.in>
2026-05-12 11:23:21 +0530

    Made changes before copying files to PARAM for testing. Moving onto BBFMM next

--- Src/LGF_test/Utils/debug.sh
+++ Src/LGF_test/Utils/debug.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
-#SBATCH --time=00:10:00
+#SBATCH --time=00:15:00
 #SBATCH --job-name=LGF_solver_test
 #SBATCH --error=job.%J.err_node_48
 #SBATCH --output=job.%J.out_node_48
@@ -18,7 +18,9 @@ export OMPI_MCA_btl_openib_if_include="mlx5_0:1"
 ulimit -s unlimited
 
 cd $SLURM_SUBMIT_DIR
+export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_DISPLAY_AFFINITY=true
 
 EXEC="./lgfpoisson"
 INPUTS_FILE="inputs"
